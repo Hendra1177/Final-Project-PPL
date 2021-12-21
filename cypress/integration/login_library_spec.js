3defa21f52730e40dcb5e8efe8287ad08fee013f
Hendra1177 <ahendra604@gmail.com>
2021-12-21 21:55:44 +0700

    Hendra : Login

--- cypress/integration/login_library_spec.js
+++ cypress/integration/login_library_spec.js
@@ -1,4 +1,4 @@
-//TC_LOGIN001
+// TC_LOGIN001
 context('Cypress Logout', () => {
     it('Login Skenario sukses', () => {
         cy.visit('https://library.polinema.ac.id') // mengunjungi website
@@ -6,13 +6,15 @@ context('Cypress Logout', () => {
         cy.get('form > :nth-child(1) > .w-full').type('1941720109') // cari input username kemudian otomatis isi dengan username
         cy.get('.mt-8 > .w-full').type('hendra11') // cari input password kemudian otomatis isi dengan password
         cy.get('.mt-10 > .flex').click() // klik login
+        cy.get('.h-full > .flex-col').should('be.visible')
     })
-    it('Logout skenario', ()=>{
-        cy.get('.ml-auto > .relative > .hidden').click()// klik dropdown
-        cy.get(':nth-child(6) > .flex > .ml-3').click() // klik logout
-    })
+    // it('Logout skenario', ()=>{
+    //     cy.get('.ml-auto > .relative > .hidden').click()// klik dropdown
+    //     cy.get(':nth-child(6) > .flex > .ml-3').click() // klik logout
+        
+    // })
 })
-//TC_LOGIN002
+// TC_LOGIN002
 context('Cypress Logout', () => {
     it('Login Skenario wrong username', () => {
         cy.visit('https://library.polinema.ac.id') // mengunjungi website
@@ -20,13 +22,11 @@ context('Cypress Logout', () => {
         cy.get('form > :nth-child(1) > .w-full').type('1941720111') // cari input username kemudian otomatis isi dengan username
         cy.get('.mt-8 > .w-full').type('hendra11') // cari input password kemudian otomatis isi dengan password
         cy.get('.mt-10 > .flex').click() // klik login
+        cy.get('.h-full > .flex-col').should('be.visible')
     })
-    it('Logout skenario', ()=>{
-        cy.get('.ml-auto > .relative > .hidden').click()// klik dropdown
-        cy.get(':nth-child(6) > .flex > .ml-3').click() // klik logout
-    })
+    
 })
-//TC_LOGIN003
+// TC_LOGIN003
 context('Cypress Logout', () => {
     it('Login Skenario wrong password', () => {
         cy.visit('https://library.polinema.ac.id') // mengunjungi website
@@ -34,13 +34,11 @@ context('Cypress Logout', () => {
         cy.get('form > :nth-child(1) > .w-full').type('1941720109') // cari input username kemudian otomatis isi dengan username
         cy.get('.mt-8 > .w-full').type('12345678') // cari input password kemudian otomatis isi dengan password
         cy.get('.mt-10 > .flex').click() // klik login
+        cy.get('.h-full > .flex-col').should('be.visible')
     })
-    it('Logout skenario', ()=>{
-        cy.get('.ml-auto > .relative > .hidden').click()// klik dropdown
-        cy.get(':nth-child(6) > .flex > .ml-3').click() // klik logout
-    })
+    
 })
-//TC_LOGIN004
+// //TC_LOGIN004
 context('Cypress Logout', () => {
     it('Login Skenario wrong password & username', () => {
         cy.visit('https://library.polinema.ac.id') // mengunjungi website
@@ -48,9 +46,7 @@ context('Cypress Logout', () => {
         cy.get('form > :nth-child(1) > .w-full').type('1941720111') // cari input username kemudian otomatis isi dengan username
         cy.get('.mt-8 > .w-full').type('12345678') // cari input password kemudian otomatis isi dengan password
         cy.get('.mt-10 > .flex').click() // klik login
+        cy.get('.h-full > .flex-col').should('be.visible')
     })
-    it('Logout skenario', ()=>{
-        cy.get('.ml-auto > .relative > .hidden').click()// klik dropdown
-        cy.get(':nth-child(6) > .flex > .ml-3').click() // klik logout
-    })
+    
 })
